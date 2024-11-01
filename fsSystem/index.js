3703562ab954aebcc6074e6c68c6965ce0300297
DavidNitian <d12mondal@gmail.com>
2024-11-01 12:12:22 +0530

    many thing

--- fsSystem/index.js
+++ fsSystem/index.js
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import fs from 'fs';
 import path from 'path';
 fs.mkdir('../fsSystem/bigB',{recursive:true},(err)=>{
@@ -24,4 +25,22 @@ fs.writeFile('./fsSystem/index.txt','David Mondal','UTF8',(err,data)=>{
         console.log(data); // Print the file contents
     }
 })
+// 3.remove direcroty
+fs.rm('./fsSystem/bigB',{recursive:true,force:true},(err)=>{
+    if(err) console.log(err);
+    else
+    console.log("dir remove");
+    
+})
 
+//4.apped file.
+fs.appendFile('./fsSystem/index.txt',"\n shreya",(error,data)=>{
+    if(error) console.log(error);
+    else
+    console.log(data);
+})
+//5.unlink
+fs.unlink('./fsSystem/b.jpg',(error,data)=>{
+    if(error) console.log(error);
+    else
+    console.log(data);})
